797f9f62041b093aedafbe64caceb43b12ea9973
Ravindu De Silva <ravindushantha1995@gmail.com>
2018-09-15 20:19:10 +0530

    Merge branch 'master' of https://github.com/Chamith95/MULY-1
    # Conflicts:
    #	.gradle/4.4/fileHashes/fileHashes.bin
    #	.gradle/4.4/fileHashes/fileHashes.lock
    #	.gradle/4.4/javaCompile/classAnalysis.bin
    #	.gradle/4.4/javaCompile/javaCompile.lock
    #	.gradle/4.4/javaCompile/taskHistory.bin
    #	.gradle/4.4/taskHistory/taskHistory.bin
    #	.gradle/4.4/taskHistory/taskHistory.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/workspace.xml
    #	app/src/main/res/layout/select_teacher.xml

--- app/src/main/java/cafe/adriel/androidaudiorecorder/example/STab1Teacher.java
+++ app/src/main/java/cafe/adriel/androidaudiorecorder/example/STab1Teacher.java
@@ -1,13 +1,14 @@
 package cafe.adriel.androidaudiorecorder.example;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+//import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class STab1Teacher extends Fragment{
+public class STab1Teacher extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
