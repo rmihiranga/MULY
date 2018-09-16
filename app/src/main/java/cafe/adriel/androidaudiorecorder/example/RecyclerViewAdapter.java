9a5b40637ed941d1714bfa61146c7e4e7b92f51a
Chamith95 <chamithjay@gmail.com>
2018-09-16 17:16:16 +0530

    Merge branch 'master' of https://github.com/Chamith95/MULY-1
    # Conflicts:
    #	.gradle/4.4/fileHashes/fileHashes.bin
    #	.gradle/4.4/fileHashes/fileHashes.lock
    #	.gradle/4.4/javaCompile/classAnalysis.bin
    #	.gradle/4.4/javaCompile/javaCompile.lock
    #	.gradle/4.4/javaCompile/taskHistory.bin
    #	.gradle/4.4/taskHistory/taskHistory.bin
    #	.gradle/4.4/taskHistory/taskHistory.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.idea/workspace.xml
    #	app/src/main/res/layout/select_teacher.xml

---
+++ app/src/main/java/cafe/adriel/androidaudiorecorder/example/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package cafe.adriel.androidaudiorecorder.example;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder> {
+
+    private ArrayList<stu_selectinstrument_item> mExampleList;
+    private OnItemClickListner mListner;
+
+    public interface OnItemClickListner{
+        void onItemclick(int position);
+    }
+
+    public void setOnItemClickListner(OnItemClickListner listner){
+        mListner=listner;
+
+    }
+
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mImageView;
+        public TextView mTextView1;
+
+        public ExampleViewHolder(final View itemView, final OnItemClickListner listner) {
+            super(itemView);
+
+            mImageView= (ImageView) itemView.findViewById(R.id.st_ins_imageview);//had to be casted??
+            mTextView1= (TextView) itemView.findViewById(R.id.st_ins_textview);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) { //Automatic change
+                    if(listner !=null){
+                        int position=getAdapterPosition();
+                        if(position !=RecyclerView.NO_POSITION){
+                            listner.onItemclick(position);
+                        }
+
+                    }
+                    itemView.getContext().startActivity(new Intent(v.getContext(),MainActivity.class));
+                }
+            });
+        }
+    }
+
+    public RecyclerViewAdapter(ArrayList<stu_selectinstrument_item> exampleList){
+        mExampleList=exampleList;
+
+
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.stu_select_instrument_item,parent,false);
+        ExampleViewHolder evh=new ExampleViewHolder(v,mListner);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        stu_selectinstrument_item currentItem=mExampleList.get(position);
+        holder.mImageView.setImageResource(currentItem.getmImagerResource());
+        holder.mTextView1.setText(currentItem.getmText1());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();//Number of items
+    }
+}
